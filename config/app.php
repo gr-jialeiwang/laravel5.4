a790123bb7ea6744f16d54ae23289825907a967b
王 佳磊 <ou.karai@starttoday.jp>
2017-07-19 18:06:56 +0900

    laravel5.4 study

--- config/app.php
+++ config/app.php
@@ -173,7 +173,7 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         
@@ -181,6 +181,8 @@ return [
         Barryvdh\Debugbar\ServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Arcanedev\LogViewer\LogViewerServiceProvider::class,
+        
+        Laravel\Passport\PassportServiceProvider::class,
     ],
 
     /*
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <example></example>
+    appId: 50a287f5c2534ce7
+    key: 995c7fe144991cf0c10bdba91d1156f1
+@endsection
